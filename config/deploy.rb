8b0fb71da27c41d17d8184499678f71463bafac1
Panupong Yodwong <panupong@bananacoding.com>
2023-02-17 16:08:47 +0700

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :default_env, { rvm_bin_path: "~/.rvm/bin" }
   append :linked_files, "config/database.yml" 
 
 # Default value for linked_dirs is []
-  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
